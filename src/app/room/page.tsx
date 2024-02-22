3f5c0eb305773c32f9292adc681e0841208af2b5
Syed zain ali shah <zaingit780@gmail.com>
2024-02-23 01:35:17 +0500

    working correctly

---
+++ src/app/room/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+import "@livekit/components-styles";
+import {
+  LiveKitRoom,
+  GridLayout,
+  ParticipantTile,
+  useTracks,
+  RoomAudioRenderer,
+  ControlBar,
+} from "@livekit/components-react";
+import { useEffect, useState } from "react";
+import { Track } from "livekit-client";
+
+export default function Page() {
+  // TODO: get user input for room and name
+  const room = "quickstart-room";
+  const name = "quickstart-user";
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const resp = await fetch(
+          `/api/get-participant-token?room=${room}&username=${name}`
+        );
+        const data = await resp.json();
+        setToken(data.token);
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, []);
+
+  if (token === "") {
+    return <div>Getting token...</div>;
+  }
+
+  return (
+    <LiveKitRoom
+      video={true}
+      audio={true}
+      token={token}
+      serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
+      data-lk-theme="default"
+      style={{ height: "100dvh" }}
+    >
+      <MyVideoConference />
+      <RoomAudioRenderer />
+      <ControlBar />
+    </LiveKitRoom>
+  );
+}
+
+function MyVideoConference() {
+  const tracks = useTracks(
+    [
+      { source: Track.Source.Camera, withPlaceholder: true },
+      { source: Track.Source.ScreenShare, withPlaceholder: false },
+    ],
+    { onlySubscribed: false }
+  );
+  return (
+    <GridLayout
+      tracks={tracks}
+      style={{ height: "calc(100vh - var(--lk-control-bar-height))" }}
+    >
+      <ParticipantTile />
+    </GridLayout>
+  );
+}
